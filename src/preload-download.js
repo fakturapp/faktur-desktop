bdaf49f4007f98d8a7ce09e69d08c4d0fc0dbabb
Dany <89309539+danbenba@users.noreply.github.com>
2026-03-28 22:56:42 +0100

    feat: add close button to legacy download modal

--- src/preload-download.js
+++ src/preload-download.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('fakturDownload', {
   onStart: (callback) => ipcRenderer.on('download-start', (_, data) => callback(data)),
   onProgress: (callback) => ipcRenderer.on('download-progress', (_, data) => callback(data)),
   onComplete: (callback) => ipcRenderer.on('download-complete', (_, data) => callback(data)),
-  onError: (callback) => ipcRenderer.on('download-error', (_, data) => callback(data))
+  onError: (callback) => ipcRenderer.on('download-error', (_, data) => callback(data)),
+  close: () => ipcRenderer.send('download-window-close')
 });
